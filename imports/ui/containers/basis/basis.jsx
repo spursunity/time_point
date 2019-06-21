dceee44ce21f6d783b66664fd96611bd4cfbe1f6
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-21 13:33:44 +0300

    create new HOC - Basis, with header and Footer

---
+++ imports/ui/containers/basis/basis.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './basis.css';
+
+const Basis = (props) => (
+  <div className='basis'>
+    <header>
+      <h1 className='headerTitle'>{ props.headerText || 'TimePoint' }</h1>
+    </header>
+    { props.children }
+    <footer>Footer</footer>
+  </div>
+);
+
+Basis.propTypes = {
+  headerText: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+export default Basis;
