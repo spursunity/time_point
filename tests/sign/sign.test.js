0fddd7ab7f91ce2cc3e8840b2fdad9adba82f470
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-23 13:57:00 +0300

    add test - check user is logged and user is not logged

--- tests/sign/sign.test.js
+++ tests/sign/sign.test.js
@@ -3,22 +3,53 @@ import 'chai/register-should';
 
 import Users from '../../imports/api/users';
 
-function findUser(username, password) {
-  return Users.find({username, password}).count();
-}
-
 describe("TimePoint sign -", function () {
   describe('user actions with auth -', () => {
     if (Meteor.isServer) {
-      it("create account -", function () {
+      const username = 'AlbertJohnJackBrienLukeAntonJakson';
+      const password = 'tt92kssss';
+
+      it("create account -", () => {
         const createAccount = Meteor.server.method_handlers['users.createAccount'];
-        const username = 'Albert';
-        const password = 'tt92kssss';
 
         createAccount(username, password);
 
-        findUser(username, password).should.equal(1);
+        findUsersCount(username, password).should.equal(1);
+      });
+
+      it("user is logged -", () => {
+        const isLogged = Meteor.server.method_handlers['users.isLogged'];
+
+        isLogged(username).should.equal(true);
+      });
+
+      it("logout -", () => {
+        const logout = Meteor.server.method_handlers['users.logout'];
+
+        logout(username);
+
+        checkUserToken().should.deep.equal({ token: null, expiration: null });
+      });
+
+      it("user is not logged -", () => {
+        const isLogged = Meteor.server.method_handlers['users.isLogged'];
+
+        isLogged(username).should.equal(false);
       });
     }
   });
 });
+
+function findUsersCount(username, password) {
+  if (!username && !password) {
+    return Users.find({}).count();
+  }
+  return Users.find({username, password}).count();
+}
+
+function checkUserToken(username, password) {
+  const user = Users.findOne({username, password});
+  const { token, expiration } = user;
+
+  return { token, expiration };
+}
