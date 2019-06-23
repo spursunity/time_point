d6135bca83daa8a555d0947215abb60bbd8d2102
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-23 13:57:49 +0300

    add method - isLogged

--- imports/api/users.js
+++ imports/api/users.js
@@ -4,27 +4,39 @@ import { check } from 'meteor/check';
 
 export default Users = new Mongo.Collection('users');
 
-const crypto = require('crypto');
 
 Meteor.methods({
   'users.createAccount'(username, password) {
     check(username, String);
     check(password, String);
 
-    const now = +(new Date());
-    const day = 86400000;
-    const expiration = `${now + day}`;
-    
-    let token;
-    crypto.randomBytes(48, function(err, buffer) {
-      token = buffer.toString('hex');
-    });
+    const tokenData = createToken();
+    const userData = { username, password, ...tokenData, };
 
-    Users.insert({
-      username,
-      password,
-      token,
-      expiration,
-    });
+    Users.insert(userData);
   },
+  'users.logout'(username) {
+    check(username, String);
+
+    Users.update({ username }, { $set: { token: null, expiration: null } });
+  },
+  'users.isLogged'(username) {
+    check(username, String);
+
+    const tokenData = Users.findOne({ username }, { token: 1, expiration: 1 });
+    const { token, expiration } = tokenData;
+    const now = +(new Date());
+
+    return token !==null && expiration > now;
+  }
 });
+
+function createToken() {
+  const now = +(new Date());
+  const day = 86400000;
+  const expiration = `${now + day}`;
+
+  const token = require('crypto').randomBytes(24).toString('hex');
+
+  return { token, expiration, };
+}
