def095d4295f03010376080146b0532120cbfb49
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-23 15:52:12 +0300

    add test - username (next: un) is empty

--- tests/sign/sign.test.js
+++ tests/sign/sign.test.js
@@ -8,6 +8,7 @@ describe("TimePoint sign -", function () {
     if (Meteor.isServer) {
       const username = 'AlbertJohnJackBrienLukeAntonJakson';
       const notExistingUsername = 'kkkkkaaaaaaaarrrrrrrrrr';
+      const emptyUsername = '';
       const password = 'tt92kssss';
       const wrongPassword = 'tt92ksss'; // without one letter -s-
 
@@ -58,10 +59,16 @@ describe("TimePoint sign -", function () {
         signin(username, wrongPassword).should.equal(0);
       });
 
-      it('username is occupied', () => {
+      it('creation: username is occupied', () => {
         const createAccount = Meteor.server.method_handlers['users.createAccount'];
 
         createAccount(username, password).should.have.own.property('username');
+      });
+
+      it('creation: username is empty', () => {
+        const createAccount = Meteor.server.method_handlers['users.createAccount'];
+
+        createAccount(emptyUsername, password).should.have.own.property('username');
       });
     }
   });
