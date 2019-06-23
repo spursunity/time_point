4b6f2f0c773044f062efbddfa310356c71dde0dd
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-23 09:37:58 +0300

    add method create account

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,3 +1,30 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
 export default Users = new Mongo.Collection('users');
+
+const crypto = require('crypto');
+
+Meteor.methods({
+  'users.createAccount'(username, password) {
+    check(username, String);
+    check(password, String);
+
+    const now = +(new Date());
+    const day = 86400000;
+    const expiration = `${now + day}`;
+    
+    let token;
+    crypto.randomBytes(48, function(err, buffer) {
+      token = buffer.toString('hex');
+    });
+
+    Users.insert({
+      username,
+      password,
+      token,
+      expiration,
+    });
+  },
+});
