69a13c5626fb536d12fb23dee75269b8eee58d2f
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-23 09:38:28 +0300

    fix collections import

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import Users from '/imports/api/users';
-import Tasks from '/imports/api/tasks';
+import Users from '../imports/api/users';
+import Tasks from '../imports/api/tasks';
 
 Meteor.startup(() => {
 
