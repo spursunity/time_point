d4b8190a3916c73c79ca34488d687a0fd4850d96
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-30 10:31:22 +0300

    add middleware for SignIn and SignUp, express-session, jsonwebtoken, hash password by drupal-hash; delete SignIn and SignUp Meteor methods.

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,77 +1,103 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import session from 'express-session';
+const jwt = require('jsonwebtoken');
+const drupalHash = require('drupal-hash');
+const bodyParser = require('body-parser');
 
 export default Users = new Mongo.Collection('users');
 
+const jwtKey = 'venovat-pozornost';
+const salt = 'uplne-vpohode';
+let initialData = {};
+
+if (Meteor.isServer) {
+  WebApp.connectHandlers.use(session({
+    secret: 'peninsula',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  }));
+
+  WebApp.connectHandlers.use(bodyParser.urlencoded({ extended: false }));
+
+  WebApp.connectHandlers.use((req, res, next) => {
+    initialData = {};
+
+    if (req.session.token) initialData.hasToken = true;
+
+    next();
+  });
+
+  WebApp.connectHandlers.use(async (req, res, next) => {
+    if (req.body.username && req.body.password) {
+      const { username, password } = req.body;
+
+      initialData.warnings = req.body.copyPassword ?
+      await signUp(username, password) :
+      await signIn(username, password);
+
+      if (! initialData.warnings) {
+        req.session.token = createToken(username);
+        initialData.hasToken = true;
+      }
+    }
+
+    next();
+  });
+}
 
 Meteor.methods({
-  'users.createAccount'(username, password) {
-    check(username, String);
-    check(password, String);
-
-    const errors = checkNewUserData(username, password);
-
-    if (errors.exist) {
-      return errors;
-    }
-
-    const tokenData = createToken();
-    const userData = { username, password, ...tokenData, };
-
-    Users.insert(userData);
+  'users.checkUserInitialData'() {
+    return initialData;
   },
-  'users.logout'(username) {
-    check(username, String);
-
-    Users.update({ username }, { $set: { token: null, expiration: null } });
-  },
-  'users.isLogged'(username) {
-    check(username, String);
-
-    const tokenData = Users.findOne({ username }, { token: 1, expiration: 1 });
-    const { token, expiration } = tokenData;
-    const now = +(new Date());
-
-    if (!expiration || expiration <= now) {
-      Users.update({ username }, { $set: { token: null, expiration: null } });
-      return false;
-    }
-
-    return token !== null;
-  },
-  'users.signin'(username, password) {
-    const tokenData = createToken();
-
-    return Users.update({ username, password }, { $set: tokenData });
+  'users.removeAllUsers'() {
+    Users.remove({});
   },
 });
 
-function createToken() {
-  const now = +(new Date());
-  const day = 86400000;
-  const expiration = `${now + day}`;
+async function signIn (username, password) {
+  check(username, String);
+  check(password, String);
 
-  const token = require('crypto').randomBytes(24).toString('hex');
+  const userData = await Users.findOne({ username });
+  if (! userData) return { password: 'Check your Username or/and password' };
 
-  return { token, expiration, };
-}
+  const passwordComparison = drupalHash.checkPassword(password, userData.password);
+  if (! passwordComparison) return { password: 'Check your Username or/and password' };
+};
 
-function checkNewUserData(username, password) {
-  const errors = { exist: false };
+async function signUp (username, password) {
+  check(username, String);
+  check(password, String);
 
-  if (!! Users.findOne({ username })) {
+  const warnings = await checkNewUserData(username, password);
+
+  if (warnings) return warnings;
+
+  const hashedPassword = drupalHash.hashPassword(password);
+  const userData = { username, password: hashedPassword };
+
+  await Users.insert(userData);
+};
+
+function createToken (userData) {
+  return jwt.sign(userData, jwtKey);
+};
+
+async function checkNewUserData(username, password) {
+  const errors = {};
+
+  if (!! await Users.findOne({ username })) {
     errors.username = 'That name is already occupied';
-    errors.exist = true;
   } else if (username === '') {
     errors.username = 'Fill out this field';
-    errors.exist = true;
   }
 
   if (password.length < 6) {
     errors.password = 'Minimal 6 symbols';
-    errors.exist = true;
   }
 
-  return errors;
+  if (errors.username || errors.password) return errors;
 }
