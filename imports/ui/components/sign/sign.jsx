4e0ed10f03e589b20a0b8238e49df4fb8090b885
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-06-30 10:38:59 +0300

    delete SignIn and SignUp methods; add authError

--- imports/ui/components/sign/sign.jsx
+++ imports/ui/components/sign/sign.jsx
@@ -1,21 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import Users from '../../../api/users';
 
 import './sign.css';
 
-const Sign = ({ trySignIn }) => {
+const Sign = ({ trySignIn, redirect, authError }) => {
+  let [ username, setUsername ] = useState('');
+  let [ password, setPassword ] = useState('');
+  let [ passwordCopy, setPasswordCopy ] = useState('');
+  let [ buttonDisabled, setButtonDisabled ] = useState(true);
+
+  useEffect(() => {
+    if (username === '' || password.length < 6 || (! trySignIn && passwordCopy !== password)) {
+      setButtonDisabled(true);
+    } else {
+      setButtonDisabled(false);
+    }
+  })
+
   const elementVisibility = trySignIn ? 'invisible' : '';
   const repeatPassClass = `signField ${elementVisibility}`;
 
+  const changeUsername = (event) => {
+    const usernameText = event.target.value;
+
+    setUsername(usernameText);
+  };
+
+  const changePassword = (event) => {
+    const passwordText = event.target.value;
+
+    setPassword(passwordText);
+  };
+
+  const changePasswordCopy = (event) => {
+    const passwordCopyText = event.target.value;
+
+    setPasswordCopy(passwordCopyText);
+  };
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    if (buttonDisabled) return;
+
+    if (trySignIn) setPasswordCopy('');
+
+    event.target.submit();
+  };
+
   return (
-    <form className='signForm'>
-      <label className='signField'>Username<input type="text"/></label>
-      <label className='signField'>Password<input type="password"/></label>
-      <label className={ repeatPassClass }>Repeat Password<input type="password"/></label>
+    <form
+    className='signForm'
+    onSubmit={ submitForm }
+    method='post'
+    action='/'
+    >
+      <label className='signField'>
+        Username
+        <input
+        type="text"
+        name='username'
+        onChange={ changeUsername }
+        />
+        {
+          authError && authError.username ?
+          <p className='hint'>{ authError.username }</p> :
+          <p className='hint'></p>
+        }
+      </label>
+      <label className='signField'>
+        Password
+        <input
+        type="password"
+        name='password'
+        onChange={ changePassword }
+        />
+        {
+          authError && authError.password ?
+          <p className='hint'>{ authError.password }</p> :
+          <p className='hint'></p>
+        }
+      </label>
+      <label className={ repeatPassClass }>
+        Repeat Password
+        <input
+        type="password"
+        name='copyPassword'
+        onChange={ changePasswordCopy }
+        />
+      </label>
       {
         trySignIn ?
-        <button>Log in</button> :
-        <button>Create account</button>
+        <button
+        type='submit'
+        disabled= { buttonDisabled }
+        >
+          Log in
+        </button> :
+        <button
+        type='submit'
+        disabled={ buttonDisabled }
+        >
+          Create account
+        </button>
       }
     </form>
   );
@@ -23,6 +112,8 @@ const Sign = ({ trySignIn }) => {
 
 Sign.propTypes = {
   trySignIn: PropTypes.bool.isRequired,
+  redirect: PropTypes.func.isRequired,
+  authError: PropTypes.object.isRequired,
 };
 
 export default Sign;
