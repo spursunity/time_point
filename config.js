779409ba05898a7c134e2449d575e66d92b2b8c5
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-10 19:27:14 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,32 @@
+import envs from './envs.js';
+
+const config = {
+	envs,
+	errorMessages: {
+		WRONG_USERNAME_PASSWORD: 'Check your Username or/and password',
+		OCCUPIED_NAME: 'That name is already occupied',
+		EMPTY_USERNAME: 'Fill out this field',
+		SHORT_PASSWORD: 'Minimal 6 symbols',
+		OCCUPIED_TASK_NAME: 'You already have such a task',
+	},
+	routes: {
+		START_PAGE: '/',
+		RULE_TIMER: '/timer',
+		TIME_LOG: '/log',
+		ANY_ROUTE: '/*',
+	},
+	headerTitles: {
+		DEFAULT: 'Time Point',
+		RULE_TIMER: 'Rule your time',
+		TIME_LOG: 'Your time is here',
+		NOT_FOUND: 'Sorry, Page not found:(',
+	},
+	urls: {
+		LOGOUT: '/?logout=true',
+	},
+	keyCodes: {
+		ENTER: 13,
+	},
+}
+
+export default config;
