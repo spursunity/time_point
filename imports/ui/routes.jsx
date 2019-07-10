9cd9d744a4c23d14f3f4dbd83122216689d2d19a
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-10 19:31:13 +0300

    add config to routes

--- imports/ui/routes.jsx
+++ imports/ui/routes.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { mount } from 'react-mounter';
-
+// config file
+import config from '../../config.js';
 // route components
 import StartPage from './pages/start-page/start-page.jsx';
 import NotFound from './pages/not-found/not-found.jsx';
 import RuleTimer from './pages/rule-timer/rule-timer.jsx';
 import TimeLog from './pages/time-log/time-log.jsx';
 
-FlowRouter.route('/', {
+const { routes } = config;
+
+FlowRouter.route(routes.START_PAGE, {
   name: 'StartPage',
   action() {
     mount(StartPage)
   },
 })
 
-FlowRouter.route('/timer', {
+FlowRouter.route(routes.RULE_TIMER, {
   name: 'RuleTimer',
   action() {
     mount(RuleTimer)
   },
 })
 
-FlowRouter.route('/log', {
+FlowRouter.route(routes.TIME_LOG, {
   name: 'TimeLog',
   action() {
     mount(TimeLog)
   },
 })
 
-FlowRouter.route('/*', {
+FlowRouter.route(routes.ANY_ROUTE, {
   name: 'NotFound',
   action() {
     mount(NotFound)
