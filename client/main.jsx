11d48d4a92dc866e98dfc2373d54194c16146588
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-12 08:24:43 +0300

    get back react-router

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-
-import '../imports/ui/routes.jsx'
+import { render } from 'react-dom';
+import { Root } from '../imports/ui/Root';
 
 Meteor.startup(() => {
-
+  render(Root(), document.getElementById('react-target'));
 });
