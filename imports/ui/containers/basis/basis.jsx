2d9f711ad42838be35c6a388d0e7ed6a62a2c979
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-12 13:32:35 +0300

    add link to github in footer

--- imports/ui/containers/basis/basis.jsx
+++ imports/ui/containers/basis/basis.jsx
@@ -23,7 +23,7 @@ const Basis = (props) => {
         }
       </header>
       { props.children }
-      <footer className='basisFooter'>Footer</footer>
+      <footer className='basisFooter'>Code on <a href="https://github.com/spursunity/time_point" target="_blank">GitHub</a></footer>
     </div>
   )
 }
