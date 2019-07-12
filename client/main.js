997f1089701e02bb1a53c19d97cc24acd1991d3a
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-12 09:49:33 +0300

    import default root

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import { Root } from '../imports/ui/Root';
+import Root from '../imports/ui/Root';
 // import StartPage from '../imports/ui/pages/start-page/start-page.jsx';
 
 Meteor.startup(() => {
