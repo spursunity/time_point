87485942add97a2da75b21fad8db4e130104873f
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-12 09:54:05 +0300

    delete start page import

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
 import Root from '../imports/ui/Root';
-// import StartPage from '../imports/ui/pages/start-page/start-page.jsx';
 
 Meteor.startup(() => {
   render(<Root />, document.getElementById('react-target'));
