881351887b91eb8c2d9d0573b6b01f4f99bb25e2
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-07-12 10:59:37 +0300

    get back initial data

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,6 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
-
 import session from 'express-session';
 const bodyParser = require('body-parser');
 
@@ -8,6 +7,8 @@ import SignHelper from './helpers/sign-helper';
 import config from '../../config.js';
 
 export const Users = new Mongo.Collection('users');
+
+let initialData = {};
 
 if (Meteor.isServer) {
   try {
@@ -30,6 +31,14 @@ if (Meteor.isServer) {
       next();
     });
 
+    WebApp.connectHandlers.use((req, res, next) => {
+      initialData = {};
+
+      if (req.session.token) initialData.hasToken = true;
+
+      next();
+    });
+
     WebApp.connectHandlers.use(async (req, res, next) => {
       if (req.body.username && req.body.password) {
         const { username, password } = req.body;
@@ -37,6 +46,13 @@ if (Meteor.isServer) {
         const result = req.body.copyPassword ?
         await signHelper.signUp(username, password) :
         await signHelper.signIn(username, password);
+
+        if (result['_id']) {
+          req.session.token = signHelper.createToken(result);
+          initialData.hasToken = true;
+        } else {
+          initialData.errors = result;
+        }
       }
 
       next();
@@ -48,7 +64,7 @@ if (Meteor.isServer) {
 
 Meteor.methods({
   'users.checkUserInitialData'() {
-    return false;
+    return initialData;
   },
   'users.removeAllUsers'() {
     try {
